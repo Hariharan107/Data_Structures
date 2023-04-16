1be5d67bba365324ab3cbf4c49b7fa88bb80f0ae
Hariharan <hariharen107@gmail.com>
2023-04-16 22:36:23 +0530

    Transposed Matrix

---
+++ Matrix/TransposedMatrix.java
@@ -0,0 +1,32 @@
+package Matrix;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] matrix = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        boolean isSymmetric = true;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] != matrix[j][i]) {
+                    isSymmetric = false;
+                    break;
+                }
+            }
+            if(!isSymmetric) {
+                break;
+            }
+        }
+        if(isSymmetric) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
